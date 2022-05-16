eec472adb9019fe888f58a91b214f65272b1a142
avoronets <avoronets@naumen.ru>
2022-05-16 23:17:38 +0300

    webinar 07 async-1

--- src/main/scala/module1/07-async.scala
+++ src/main/scala/module1/07-async.scala
@@ -1,1 +1,169 @@
 package module1
+
+import java.util.concurrent.{Callable, Executor, ExecutorService, Executors, ForkJoinPool, ThreadPoolExecutor}
+import scala.collection.mutable
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.language.postfixOps
+import scala.util.{Failure, Success, Try}
+
+object threads {
+
+
+  def printRunningTime(v: => Unit): Unit = {
+    val start = System.currentTimeMillis()
+    v
+    val end = System.currentTimeMillis()
+    println(s"Execution time ${end -start}")
+  }
+
+  def getRatesLocation1: Thread = async({
+    Thread.sleep(1000)
+    println("GetLocation1")
+  })
+
+  def getRatesLocation2: Thread = async({
+    Thread.sleep(2000)
+    println("GetLocation2")
+  })
+
+  def async(f: => Unit): Thread = {
+    new Thread{
+      override def run(): Unit = f
+    }
+  }
+
+  def async2[A](f: => A): A = {
+    var v: A = null.asInstanceOf[A]
+
+    val t = new Thread{
+      override def run(): Unit = {
+        v = f
+      }
+    }
+    t.start()
+    t.join()
+    v
+  }
+
+  def getRatesLocation3: Int = async2({
+    Thread.sleep(1000)
+    println("GetLocation1")
+    10
+  })
+
+  def getRatesLocation4: Int = async2({
+    Thread.sleep(2000)
+    println("GetLocation2")
+    20
+  })
+
+
+  class ToyFuture[T] private (v: () => T){
+
+    private var isCompleted: Boolean = false
+    private var r: T = null.asInstanceOf[T]
+    private val q = mutable.Queue[T => _]()
+
+
+    def map[B](f: T => B): ToyFuture[B] = ???
+    def flatMap[B](f: T => ToyFuture[B]): ToyFuture[B] = ???
+
+    def onComplete[U](f: T => U): Unit = {
+      if(isCompleted) f(r)
+      else q.enqueue(f)
+    }
+
+    private def start() = {
+      val t = new Thread{
+        override def run(): Unit = {
+          val result = v()
+          r = result
+          isCompleted = true
+          while (q.nonEmpty){
+            q.dequeue()(result)
+          }
+        }
+      }
+      t.start()
+    }
+  }
+
+  object ToyFuture {
+    def apply[T](v: => T): ToyFuture[T] = {
+      val f = new ToyFuture[T](() => v)
+      f.start()
+      f
+    }
+  }
+
+  def getRatesLocation5: ToyFuture[Int] = ToyFuture({
+    Thread.sleep(1000)
+    println("GetLocation1")
+    10
+  })
+
+  def getRatesLocation6: ToyFuture[Int] = ToyFuture({
+    Thread.sleep(2000)
+    println("GetLocation2")
+    20
+  })
+
+
+
+
+
+  class Thread1 extends Thread {
+    override def run(): Unit = {
+      Thread.sleep(1000)
+      println(s"Hello from ${Thread.currentThread().getName()}")
+    }
+  }
+
+}
+
+
+
+
+
+
+
+object executor {
+
+      val pool1: ExecutorService = Executors.newFixedThreadPool(2)
+      val pool2: ExecutorService = Executors.newCachedThreadPool()
+      val pool3: ExecutorService = Executors.newWorkStealingPool(4)
+
+
+}
+
+
+object future{
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val f1 = Future(2 + 2)
+  val f2 = Future.failed(new Exception("Ooops"))
+  val f3 = Future.successful(2 + 2)
+  val f4 = Future.fromTry(Try(1 / 0))
+
+  val v = f1.map(i => i + 1)
+
+  for{
+    v1 <- Future(2 + 2)
+    v2 <- Future(2 + 2)
+  } yield v1 + v2
+
+  val r: Unit = f1.onComplete {
+    case Failure(exception) => println(exception.getMessage)
+    case Success(value) => value + 1
+  }
+
+  val r2: Int = Await.result(f1, 5 second)
+  val r3: Future[Int] = Await.ready(f1, 5 second)
+
+
+
+
+
+}
--- src/main/scala/module1/App.scala
+++ src/main/scala/module1/App.scala
@@ -1,27 +1,74 @@
 package module1
 
+import java.util.concurrent.{Callable, Future}
+
 object App {
   def main(args: Array[String]): Unit = {
 
-//    //val two = (x: Int) => { return x; 2 }
-//    def sumItUp: Int = {
-//      def one(x: Int): Int = { return x; 1 }
-//      def two(x: Int): Int = { return x; 2 }
-//      1 + one(2) + two(11)
-//    }
-//
-//    println(sumItUp)
-//
-//    println("Hello world")
 
-    def doomy(a: String): Unit = {
-      Thread.sleep(1000)
-      println(a)
+    println(s"Hello from ${Thread.currentThread().getName()}")
+
+//    val t1 = new threads.Thread1
+//    val t2 = new threads.Thread1
+//
+//    t1.start()
+//    t1.join()
+//    t2.start()
+
+    def rates = {
+      val t1 = threads.getRatesLocation1
+      val t2 = threads.getRatesLocation2
+      t1.start()
+      t2.start()
+      t1.join()
+      t2.join()
     }
 
-    val r = hof.logRunningTime(doomy)
+    def rates2 = {
+      val t1: Int = threads.getRatesLocation3
+      val t2: Int = threads.getRatesLocation4
+      println(t1)
+      println(t2)
+    }
 
-    r("Hello")
-    //println(hof.logRunningTime(doomy()))
+    def rates3 = {
+      val t1 = threads.getRatesLocation5
+      val t2 = threads.getRatesLocation6
+
+      val t3: threads.ToyFuture[Int] = for{
+        v1 <- t1
+        v2 <- t2
+      } yield v1 + v2
+
+      t1.onComplete(i => println(s"Hello from future ${Thread.currentThread().getName} - $i"))
+      t2.onComplete(i => println(s"Hello from future ${Thread.currentThread().getName} - $i"))
+    }
+
+   // threads.printRunningTime(rates)
+   // threads.printRunningTime(rates2)
+   // threads.printRunningTime(rates3)
+
+    val runnable = new Runnable {
+      override def run(): Unit = {
+        Thread.sleep(1000)
+        println(s"Hello from runnable - ${Thread.currentThread().getName}")
+      }
+    }
+    val callable = new Callable[Int] {
+      override def call(): Int = {
+        Thread.sleep(1000)
+        println("Hello from Callable")
+        10 + 10
+      }
+    }
+
+    // executor.pool3.execute(runnable)
+    val f1: Future[Int] = executor.pool3.submit(callable)
+
+   // val i: Int = f1.get()
+  //  println(i)
+    println("after future get")
+    //executor.pool1.shutdown()
+   Thread.sleep(2000)
   }
 }
